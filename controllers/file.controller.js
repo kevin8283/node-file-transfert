e320ba5c94426c55300264be2f09777458898371
kevin <keyvinkeygen@gmail.com>
2022-04-24 04:55:37 +0200

    fix check dir types

--- controllers/file.controller.js
+++ controllers/file.controller.js
@@ -13,47 +13,58 @@ const fileController = {
             if (error) {
                 return res.json({ error: `Error reading path ${path}` })
             }
-            
+
             files.forEach((file, index) => {
                 let fileObject = {
                     index: index,
                     name: file
-                } 
-
-                const regex = new RegExp('\.[0-9a-z]+$', 'i')
-                const extension = file.match(regex)[0]
-
-                if (extension === file) {
-                    fileObject = {...fileObject, type: 'dir'}
                 }
-                else {
-                    const audioExtensions = [".mp3", ".wma", ".ape", ".flac", ".wav"]
-                    const videoExtensions = [".mp4", ".mkv", ".webm", ".mov", ".ogg", ".flv", ".avi", ".3gp"]
-                    const imageExtensions = [".webp", ".jpeg", ".jpg", ".png", ".gif", ".bmp", ".ico"]
-                    const fileImageExtensions = [".iso", ".daa"]
-                    const compressedExtensions = [".rar", ".zip", ".7zip"]
-                    const executableExtensions = [".exe"]
 
-                    if (audioExtensions.includes(extension.toLocaleLowerCase())) {
-                        fileObject = {...fileObject, type: 'audio'}
-                    }
-                    else if (videoExtensions.includes(extension.toLocaleLowerCase())) {
-                        fileObject = {...fileObject, type: 'video'}
-                    }
-                    else if (imageExtensions.includes(extension.toLocaleLowerCase())) {
-                        fileObject = {...fileObject, type: 'image'}
-                    }
-                    else if (fileImageExtensions.includes(extension.toLocaleLowerCase())) {
-                        fileObject = {...fileObject, type: 'file-image'}
-                    }
-                    else if (compressedExtensions.includes(extension.toLocaleLowerCase())) {
-                        fileObject = {...fileObject, type: 'compressed'}
-                    }
-                    else if (executableExtensions.includes(extension.toLocaleLowerCase())) {
-                        fileObject = {...fileObject, type: 'executable'}
+                const regex = new RegExp('\.[0-9a-z]+$', 'i', 'g')
+
+                if (file.match(regex) === null) {
+                    fileObject = fileObject = { ...fileObject, type: 'dir' }
+                }
+
+                else {
+                    if (!file.match(regex)[0].includes('.')) {
+                        fileObject = { ...fileObject, type: 'dir' }
                     }
                     else {
-                        fileObject = {...fileObject, type: 'file'}
+                        const extension = file.match(regex)[0]
+
+                        const audioExtensions = [".mp3", ".wma", ".ape", ".flac", ".wav"]
+                        const videoExtensions = [".mp4", ".mkv", ".webm", ".mov", ".ogg", ".flv", ".avi", ".3gp"]
+                        const imageExtensions = [".webp", ".jpeg", ".jpg", ".png", ".gif", ".bmp", ".ico"]
+                        const fileImageExtensions = [".iso", ".daa"]
+                        const compressedExtensions = [".rar", ".zip", ".7zip"]
+                        const executableExtensions = [".exe"]
+                        const documentExtensions = [".pdf", ".doc", ".docx", ".txt"]
+
+                        if (audioExtensions.includes(extension.toLocaleLowerCase())) {
+                            fileObject = { ...fileObject, type: 'audio' }
+                        }
+                        else if (videoExtensions.includes(extension.toLocaleLowerCase())) {
+                            fileObject = { ...fileObject, type: 'video' }
+                        }
+                        else if (imageExtensions.includes(extension.toLocaleLowerCase())) {
+                            fileObject = { ...fileObject, type: 'image' }
+                        }
+                        else if (fileImageExtensions.includes(extension.toLocaleLowerCase())) {
+                            fileObject = { ...fileObject, type: 'file-image' }
+                        }
+                        else if (compressedExtensions.includes(extension.toLocaleLowerCase())) {
+                            fileObject = { ...fileObject, type: 'compressed' }
+                        }
+                        else if (executableExtensions.includes(extension.toLocaleLowerCase())) {
+                            fileObject = { ...fileObject, type: 'executable' }
+                        }
+                        else if (documentExtensions.includes(extension.toLocaleLowerCase())) {
+                            fileObject = { ...fileObject, type: 'document' }
+                        }
+                        else {
+                            fileObject = { ...fileObject, type: 'file' }
+                        }
                     }
                 }
 
@@ -99,26 +110,25 @@ const fileController = {
     },
 
     uploadFile: (req, res) => {
-        const downloadPath = path.relative(__dirname,  'C:/Downloads')
-        console.log(downloadPath)
+        const downloadPath = path.relative(__dirname, 'C:/Downloads')
 
         const storage = multer.diskStorage({
             destination: (req, file, callback) => {
                 callback(null, downloadPath)
             },
-            filename: (req, file, cb) => { 
-                cb(null , file.originalname)   
+            filename: (req, file, cb) => {
+                cb(null, file.originalname)
             }
         })
 
-        const upload = multer({storage: storage}).single("file")
+        const upload = multer({ storage: storage }).single("file")
 
         upload(req, res, (err) => {
             if (err) {
                 console.log(err)
                 res.status(400).send("Something went wrong!")
             }
-            res.send(req.file)
+            res.json({ message: "File uploaded successfully" })
         });
     }
 }
